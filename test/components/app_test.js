dc8d5d87abffa84c04dcc62ab40a7d1d49c2725f
Xingjian <i@xingjian.me>
2017-02-13 21:24:10 +0800

    till lesson 11

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,25 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
+
+// use 'descibe' to group simliar tests
 describe('App' , () => {
   let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
 
+  component = renderComponent(App);
+
+
+  // use 'it' to test a single attribute of a target
   it('renders something', () => {
+    // use 'expect' to make an 'assertion' about a target
     expect(component).to.exist;
   });
+
+  it('shows the correct text', ()=>{
+    expect(component).to.contain('React simple starter');
+  })
 });
+
+
+
