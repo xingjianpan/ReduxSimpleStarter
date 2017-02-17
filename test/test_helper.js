98abfc10764c7b49a79d372523b160bef16a31f3
Xingjian <i@xingjian.me>
2017-02-17 21:27:03 +0800

    update

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,36 +1,48 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
-import jsdom from 'jsdom';
-import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from '../src/reducers';
+import jsdom from 'jsdom'
+import _$ from 'jquery'
+import TestUtils from 'react-addons-test-utils'
+import ReactDOM from 'react-dom'
+import chai, {expect} from 'chai'
+import React from 'react'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import reducers from '../src/reducers'
+import chaiJquery from 'chai-jquery'
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
 
-chaiJquery(chai, chai.util, $);
+// set up testing enviroment to run like a browser in the command line
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
+global.window = global.document.defaultView
+// tell juqery to get access to global.window proived by jsdom
+const $ = _$(global.window)
 
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
+// build 'renderComponent' helper that should render a given react class
+function renderComponent(ComponentClass, props, state){
+  const ComponentInstance = TestUtils.renderIntoDocument(
+    <Provider store = {createStore(reducers, state)}>
       <ComponentClass {...props} />
     </Provider>
-  );
+    )
 
-  return $(ReactDOM.findDOMNode(componentInstance));
+  return $(ReactDOM.findDOMNode(ComponentInstance)) //produce HTML
+
 }
 
-$.fn.simulate = function(eventName, value) {
-  if (value) {
-    this.val(value);
-  }
-  TestUtils.Simulate[eventName](this[0]);
-};
 
-export {renderComponent, expect};
+
+// build helper for simulating events
+$.fn.simulate = function(eventName, value){
+
+  if (value){
+    this.val(value)
+  }
+
+  TestUtils.Simulate[eventName](this[0])
+
+}
+
+
+
+// set up chai-query
+chaiJquery(chai, chai.util, $)
+export {renderComponent, expect}
